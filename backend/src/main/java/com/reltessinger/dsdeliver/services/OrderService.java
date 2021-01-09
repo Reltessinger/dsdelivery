732370c7f877c63bdd477784c69cd97e295cf7b7
Rafael Reltessinger <rafael.reltessinger@outlook.com>
2021-01-09 16:59:05 +0000

    endpoint POST /orders

--- backend/src/main/java/com/reltessinger/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/reltessinger/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.reltessinger.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,18 +9,38 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.reltessinger.dsdeliver.dto.OrderDTO;
+import com.reltessinger.dsdeliver.dto.ProductDTO;
 import com.reltessinger.dsdeliver.entities.Order;
+import com.reltessinger.dsdeliver.entities.OrderStatus;
+import com.reltessinger.dsdeliver.entities.Product;
 import com.reltessinger.dsdeliver.repositories.OrderRepository;
+import com.reltessinger.dsdeliver.repositories.ProductRepository;
 
- @Service
+@Service
 public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
 	
-	 @Autowired
-	 private OrderRepository orderRepository;
-	 
-	 @Transactional(readOnly = true)
-	 public List<OrderDTO> findAll(){
-		 List<Order> allProducts = orderRepository.findOrdersWithProducts();
-		 return allProducts.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
-	 }
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> allProducts = orderRepository.findOrdersWithProducts();
+		return allProducts.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
+	}
+
 }
